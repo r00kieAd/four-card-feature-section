4f096f64e7d487d2991280e1cc5029af3dc99b60
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2023-11-27 23:43:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+$('.team-builder').css("border-top", "5px solid hsl(0, 78%, 62%)");
+$('.calculator').css("border-top", "5px solid hsl(180, 62%, 55%)");
+$('.karma').css("border-top", "5px solid hsl(34, 97%, 64%)");
+$('.supervisor').css("border-top", "5px solid hsl(212, 86%, 64%)");
+
+$(".team-builder").hover(function() {
+    $('.team-builder').toggleClass("neon-a");
+});
+
+$("div.calculator").hover(function() {
+    $('.calculator').toggleClass("neon-b");
+});
+
+$("div.karma").hover(function() {
+    $('.karma').toggleClass("neon-c");
+});
+
+$(".supervisor").hover(function() {
+    $('.supervisor').toggleClass("neon-d");
+});
+
+// if (screen.width <= 1000) {
+//     $('div.center-components').addClass('mobile-components');
+//     $('div.mobile-components').removeClass('center-components');
+// }

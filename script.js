b2eb1e8896e091db11814f82935b4f2fa04b3818
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2023-11-30 11:21:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
 try {
-$('.team-builder').css("border-top", "5px solid hsl(0, 78%, 62%)");
-$('.calculator').css("border-top", "5px solid hsl(180, 62%, 55%)");
-$('.karma').css("border-top", "5px solid hsl(34, 97%, 64%)");
-$('.supervisor').css("border-top", "5px solid hsl(212, 86%, 64%)");
-
-$(".team-builder").hover(function() {
-    $('.team-builder').toggleClass("neon-a");
-});
-
-$("div.calculator").hover(function() {
-    $('.calculator').toggleClass("neon-b");
-});
-
-$("div.karma").hover(function() {
-    $('.karma').toggleClass("neon-c");
-});
-
-$(".supervisor").hover(function() {
-    $('.supervisor').toggleClass("neon-d");
-    $('.supervisor div img').css('display', 'none');
-    $('.supervisor lord-icon').css('display', 'block');
+    $('.team-builder').css("border-top", "5px solid hsl(0, 78%, 62%)");
+    $('.calculator').css("border-top", "5px solid hsl(180, 62%, 55%)");
+    $('.karma').css("border-top", "5px solid hsl(34, 97%, 64%)");
+    $('.supervisor').css("border-top", "5px solid hsl(212, 86%, 64%)");
+    
+    $(".team-builder").hover(function() {
+        $('.team-builder').toggleClass("neon-a");
+    });
+    
+    $("div.calculator").hover(function() {
+        $('.calculator').toggleClass("neon-b");
+    });
+    
+    $("div.karma").hover(function() {
+        $('.karma').toggleClass("neon-c");
+    });
+    
+    $(".supervisor").hover(function() {
+        $('.supervisor').toggleClass("neon-d");
+        $('.supervisor div img').css('display', 'none');
+        $('.supervisor lord-icon').css('display', 'block');
+    }
 } catch (err) {
     console.log(err.message);
 }

514472a8542c472dfc8b0de72f5ab5e4c3b22ec9
Adhyatma Dwivedi <113773621+r00kieAd@users.noreply.github.com>
2023-11-30 15:34:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,34 +5,38 @@ try {
     $('.supervisor').css("border-top", "5px solid hsl(212, 86%, 64%)");
     
     $(".team-builder").hover(function() {
-        $('.team-builder').toggleClass("neon-a");
+        $('.team-builder').addClass("neon-a");
         $('.team-builder > .icon > lord-icon').attr('trigger', 'loop');
         $('.team-builder > .icon > lord-icon').attr('delay', '1');
     }, function(){
+        $('.team-builder').removeClass("neon-a");
         $('.team-builder > .icon > lord-icon').attr('trigger', 'click');
     });
     
     $("div.calculator").hover(function() {
-        $('.calculator').toggleClass("neon-b");
+        $('.calculator').addClass("neon-b");
         $('.calculator > .icon > lord-icon').attr('trigger', 'loop');
         $('.calculator > .icon > lord-icon').attr('delay', '1');
     }, function(){
+        $('.calculator').removeClass("neon-b");
         $('.calculator > .icon > lord-icon').attr('trigger', 'click');
     });
     
     $("div.karma").hover(function() {
-        $('.karma').toggleClass("neon-c");
+        $('.karma').addClass("neon-c");
         $('.karma > .icon > lord-icon').attr('trigger', 'loop');
         $('.karma > .icon > lord-icon').attr('delay', '1');
     }, function(){
+        $('.karma').addClass("neon-c");
         $('.karma > .icon > lord-icon').attr('trigger', 'click');
     });
     
     $(".supervisor").hover(function() {
-        $('.supervisor').toggleClass("neon-d");
+        $('.supervisor').addClass("neon-d");
         $('.supervisor > .icon > lord-icon').attr('trigger', 'loop');
         $('.supervisor > .icon > lord-icon').attr('delay', '1');
     }, function() {
+        $('.supervisor').removeClass("neon-d");
         $('.supervisor > .icon > lord-icon').attr('trigger', 'click');
     });
 } catch (err) {
